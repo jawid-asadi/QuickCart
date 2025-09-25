dfc50f3abc5f9c9a9ba319374d53835723a7b98c
jawid-asadi <javidasadi622@gmail.com>
2025-09-25 09:44:06 +0430

    first commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: {type: String, require: true},
+    name: {type: String, require: true},
+    email: {type: String, require: true, unique: true},
+    imageUrl: {type: String, require: true},
+    cartItem: {type: Object, default: {}},
+}, {minimize: false})
+
+const User = mongoose.model.user || mongoose.model('user', userSchema);
+
+export default User;
